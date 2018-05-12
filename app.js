e4491a2dc9426078f161df965a88d32c1a679b16
Adrian Lau <megree@adrians-mbp.mynet>
2018-05-12 12:31:07 +0800

    Added queue, update database on unload

--- app.js
+++ app.js
@@ -11,39 +11,53 @@ var canvasSchema = new mongoose.Schema({
 var canvas = mongoose.model('canvas', canvasSchema);
 // canvas({pixels: Array(64 * 64).fill(0)}).save();
 
+var queue = [];
+
 app.use(express.static('./public'));
 
 app.get('/', function(req, res){
-
   res.sendFile(__dirname + '/views/index.html');
 });
 
 io.on('connection', function(socket) {
-
   console.log('a user connected');
 
   // <filter> is {'_id': '5aedbfa63e58ad3f74d92862'}
   socket.on('init', function(req) {
-    canvas.find({}, function(err, data) {
-      console.log('event: init');
-      io.to(req.id).emit('init', data[0].pixels);
-    });
+    queue.push(req);
+    console.log(queue);
+    if (queue.length == 1) {
+      canvas.find({}, function(err, data) {
+        io.to(req.id).emit('init', data[0].pixels);
+      });
+      return;
+    }
+    io.to(queue[0].id).emit('ask', {});
+  });
+
+  socket.on('answer', function(pagePixels) {
+    io.to(queue[queue.length - 1].id).emit('init', pagePixels.pixels);
   });
 
   socket.on('draw', function(pixel) {
-    console.log('event: draw');
     io.emit('draw', pixel);
-    canvas.find({}, function(err, data) {
-      pixels = data[0].pixels;
-      pixels[pixel.pos] = pixel.color;
-      canvas.update(
-         {},
-         { $set: {"pixels": pixels} },
-         function(err, numAffected) {
-           return;
-         }
-      );
+  });
+
+  socket.on('save', function(data) {
+    console.log('heehee! session id', data.id);
+    if (!data.pixels || data.pixels.length !== 6) return;
+    queue = queue.filter(function(req) {
+      return req.id != data.id;
     });
+    console.log('new queue', queue);
+    if (queue.length != 0) return;
+    canvas.update(
+       {},
+       { $set: {"pixels": data.pixels} },
+       function(err, numAffected) {
+         return;
+       }
+    );
   });
 
 });
--- public/place.js
+++ public/place.js
@@ -1,8 +1,11 @@
 $(function () {
-  var colors = ['white', 'red', 'blue', 'yellow', 'green', 'brown'];
+  var colors = ['white', 'lightgrey', 'grey', 'black', 'pink', 'red', 'orange', 'brown', 'yellow', 'lightgreen', 'green', 'Aquamarine', 'Aqua', 'blue', 'violet', 'purple'];
   var swatch = 0;
 
   var socket = io();
+  var sessionID = null;
+
+  var pagePixels = [];
 
   $('.palette').children('.color').each(function (i) {
     $(this).css('background-color', colors[i]).click(function() {
@@ -11,29 +14,41 @@ $(function () {
   });
 
   socket.on('connect', function() {
+    sessionID = socket.id;
 
     socket.emit('init', {id: socket.id});
     socket.on('init', function(pixels) {
       console.log('event: init');
       len = pixels.length;
-      for (var i = 0; i < len; i++)
+      for (var i = 0; i < len; i++) {
         $('.canvas').append('<div class="pixel" style="background-color:' + colors[pixels[i]] + ';"></div>');
+        pagePixels[i] = pixels[i];
+      }
+    });
+
+    socket.on('ask', function() {
+      console.log('im asked');
+      socket.emit('answer', {pixels: pagePixels});
     });
 
     $('.canvas').on('click', '.pixel', function() {
       $(this).css('background-color', colors[swatch]);
-      console.log({pos: $(this).index(), color: swatch});
-      socket.emit('draw', {pos: $(this).index(), color: swatch});
+      var pixel = {pos: $(this).index(), color: swatch};
+      pagePixels[pixel.pos] = pixel.color;
+      socket.emit('draw', pixel);
     });
 
     socket.on('draw', function(pixel) {
-      console.log('sync');
       console.log(pixel);
+      pagePixels[pixel.pos] = pixel.color;
       //$('.canvas .pixel:nth-child(' + pixel.pos + ')').css('background-color', 'red');
       $(".pixel:eq(" + pixel.pos + ")").css('background-color', colors[pixel.color]);
     });
 
   });
 
+  $(window).on('beforeunload', function(){
+    socket.emit('save', {pixels: pagePixels, id: sessionID});
+  });
 
 });
