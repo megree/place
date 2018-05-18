1f121b068a9dc2b596e03be737ef9db42b4815aa
Adrian Lau <megree@adrians-mbp.mynet>
2018-05-19 05:55:46 +0800

    Added local pixels storage and test folder for canvas init

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ var canvasSchema = new mongoose.Schema({
 var canvas = mongoose.model('canvas', canvasSchema);
 // canvas({pixels: Array(64 * 64).fill(0)}).save();
 
-var queue = [];
-
 app.use(express.static('./public'));
 
 app.get('/', function(req, res){
@@ -21,43 +19,36 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket) {
   console.log('a user connected');
-
   // <filter> is {'_id': '5aedbfa63e58ad3f74d92862'}
   socket.on('init', function(req) {
-    queue.push(req);
-    console.log(queue);
-    if (queue.length == 1) {
-      canvas.find({}, function(err, data) {
-        io.to(req.id).emit('init', data[0].pixels);
-      });
-      return;
-    }
-    io.to(queue[0].id).emit('ask', {});
-  });
-
-  socket.on('answer', function(pagePixels) {
-    io.to(queue[queue.length - 1].id).emit('init', pagePixels.pixels);
-  });
-
-  socket.on('draw', function(pixel) {
-    io.emit('draw', pixel);
-  });
-
-  socket.on('save', function(data) {
-    console.log('heehee! session id', data.id);
-    if (!data.pixels || data.pixels.length !== 6) return;
-    queue = queue.filter(function(req) {
-      return req.id != data.id;
+    canvas.find({}, function(err, data) {
+      io.to(req.id).emit('init', data[0].pixels);
     });
-    console.log('new queue', queue);
-    if (queue.length != 0) return;
+  });
+
+  socket.on('draw', function(data) {
+    var timestart = Date.now();
+    io.emit('draw', data.pixel);
+    if (data.pixels.length != 100 * 100) return;
     canvas.update(
        {},
        { $set: {"pixels": data.pixels} },
        function(err, numAffected) {
-         return;
+         console.log('processing time for database update is', Date.now() / 1000 - timestart / 1000, 's');
        }
     );
+    /*
+    canvas.find({}, function(err, data) {
+      data[0].pixels[pixel.pos] = pixel.color;
+      canvas.update(
+         {},
+         { $set: {"pixels": data[0].pixels} },
+         function(err, numAffected) {
+           console.log('processing time is', Date.now() / 1000 - timestart / 1000, 's');
+         }
+      );
+    });
+    */
   });
 
 });
--- public/place.js
+++ public/place.js
@@ -1,14 +1,21 @@
 $(function () {
-  var colors = ['white', 'lightgrey', 'grey', 'black', 'pink', 'red', 'orange', 'brown', 'yellow', 'lightgreen', 'green', 'Aquamarine', 'Aqua', 'blue', 'violet', 'purple'];
+  /* var colors = ['white', 'lightgrey', 'grey', 'black', 'pink', 'red',
+  'orange', 'brown', 'yellow', 'lightgreen', 'green', 'Aquamarine',
+  'Aqua', 'blue', 'violet', 'purple']; */
+  var colors = [[255, 255, 255], [228, 228, 228], [136, 136, 136], [34, 34, 34], [255, 167, 209], [229, 0, 0],
+  [229, 149, 0], [160, 106, 66], [229, 217, 0], [148, 224, 68], [2, 190, 1], [0, 211, 211],
+  [0, 131, 199], [0, 0, 234], [207, 110, 228], [130, 0, 128]];
   var swatch = 0;
 
   var socket = io();
   var sessionID = null;
 
+  var isTimeout = false;
+
   var pagePixels = [];
 
   $('.palette').children('.color').each(function (i) {
-    $(this).css('background-color', colors[i]).click(function() {
+    $(this).css('background-color', 'rgb(' + colors[i] + ')').click(function() {
       swatch = $(this).index();
     });
   });
@@ -21,34 +28,47 @@ $(function () {
       console.log('event: init');
       len = pixels.length;
       for (var i = 0; i < len; i++) {
-        $('.canvas').append('<div class="pixel" style="background-color:' + colors[pixels[i]] + ';"></div>');
+        $('.canvas').append('<div class="pixel" style="background-color:rgb(' + colors[pixels[i]] + ');"></div>');
         pagePixels[i] = pixels[i];
       }
     });
 
-    socket.on('ask', function() {
-      console.log('im asked');
-      socket.emit('answer', {pixels: pagePixels});
-    });
-
     $('.canvas').on('click', '.pixel', function() {
+      if (isTimeout) return;
       $(this).css('background-color', colors[swatch]);
       var pixel = {pos: $(this).index(), color: swatch};
-      pagePixels[pixel.pos] = pixel.color;
-      socket.emit('draw', pixel);
+      pagePixels[$(this).index()] = swatch;
+      socket.emit('draw', {pixel: pixel, pixels: pagePixels});
+      startTimeout(15);
     });
 
     socket.on('draw', function(pixel) {
       console.log(pixel);
-      pagePixels[pixel.pos] = pixel.color;
-      //$('.canvas .pixel:nth-child(' + pixel.pos + ')').css('background-color', 'red');
-      $(".pixel:eq(" + pixel.pos + ")").css('background-color', colors[pixel.color]);
+      $(".pixel:eq(" + pixel.pos + ")").css('background-color', 'rgb(' + colors[pixel.color] + ')');
     });
 
   });
 
-  $(window).on('beforeunload', function(){
-    socket.emit('save', {pixels: pagePixels, id: sessionID});
-  });
+  function startTimeout(duration) {
+    isTimeout = true;
+
+    $('.timer').show();
+    var minute = Math.floor(duration / 60);
+    var second = duration - minute * 60
+    $('.timer').text(minute + ':' + (second < 10? '0' + second : second));
+    duration--;
+
+    var setIntervalID = setInterval(function() {
+      minute = Math.floor(duration / 60);
+      second = duration - minute * 60
+      $('.timer').text(minute + ':' + (second < 10? '0' + second : second));
+      duration--;
+      if (duration < -1) {
+        clearInterval(setIntervalID);
+        isTimeout = false;
+        $('.timer').hide();
+      }
+    }, 1000)
+  }
 
 });
